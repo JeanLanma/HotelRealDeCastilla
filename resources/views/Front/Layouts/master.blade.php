0bf4d49f4d7b0195cbc7e7154fc16d3f20fd2535
Jean <jeanmacario048@gmail.com>
2024-07-11 10:07:05 -0600

    Add\ Favicons

--- resources/views/Front/Layouts/master.blade.php
+++ resources/views/Front/Layouts/master.blade.php
@@ -9,6 +9,15 @@
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap" rel="stylesheet">
 
+  <!-- Favicons -->
+  <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="favicons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="favicons/favicon-16x16.png">
+    <!-- <link rel="manifest" href="/site.webmanifest"> -->
+    <!-- <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5"> -->
+    <meta name="msapplication-TileColor" content="#9b111e">
+    <meta name="theme-color" content="#9b111e">
+
     <title>
     @if(View::hasSection('title'))
       @yield('title') - {{ config('app.name', 'Laravel') }}
