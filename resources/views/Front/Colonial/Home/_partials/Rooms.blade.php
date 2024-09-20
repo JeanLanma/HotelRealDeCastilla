2b295586162f7b0ad718b06adf51e4fc7f573122
Jean <jeanmacario048@gmail.com>
2024-09-20 13:42:33 -0600

    Add\ All Rooms Colonial

--- resources/views/Front/Colonial/Home/_partials/Rooms.blade.php
+++ resources/views/Front/Colonial/Home/_partials/Rooms.blade.php
@@ -148,6 +148,119 @@
                 </div>
             </div>
         </div>
+
+        
+        <div class="swiper-slide">
+            <div class="flex flex-row-reverse justify-between items-start min-h-screen">
+                <div class="bg-primary-dark w-1/2 text-primary-light">
+                    <div class="w-full h-screen pl-20 pt-[6.2rem]">
+
+                        <div class=" flex items-baseline text-[.6rem] text-primary-gray mb-6">
+                            <span><img src="{{asset('icons/Line.svg')}}" alt=""></span>
+                            <p class="ml-3">2 Huespedes</p>
+                        </div>
+                        
+                        <h2 class="text-4xl font-semibold tracking-wide mb-[5rem] font-playfair">
+                            Habitación Estándar Queen
+                        </h2>
+
+                        <ul class="list-disc text-sm pl-4 mb-[5rem]">
+                            <li class="mt-2">1 camas queen size</li>
+                            <li class="mt-2">TV por cable</li>
+                            <li class="mt-2">Teléfono</li>
+                            <li class="mt-2">Wifi</li>
+                            <li class="mt-2">Aire acondicionado</li>
+                            <li class="mt-2">Baño completo con todas sus amenidades</li>
+                        </ul>
+
+                        <div>
+                            <button class="text-xs font-semibold uppercase border border-white rounded-full px-5 py-3 hover:text-primary-dark hover:bg-white transition-colors duration-200">Reservar ahora</button>
+                        </div>
+
+                    </div>
+                </div>
+                <div class="w-1/2">
+                    <div class="w-full h-screen">
+                        <img class="w-full h-screen object-cover" src="{{asset('images/habitaciones/habitacion-estandar-queen-02.jpg')}}" alt="Habitacion doble queen">
+                    </div>
+                </div>
+            </div>
+        </div>
+                
+        <div class="swiper-slide">
+            <div class="flex flex-row-reverse justify-between items-start min-h-screen">
+                <div class="bg-primary-dark w-1/2 text-primary-light">
+                    <div class="w-full h-screen pl-20 pt-[6.2rem]">
+
+                        <div class=" flex items-baseline text-[.6rem] text-primary-gray mb-6">
+                            <span><img src="{{asset('icons/Line.svg')}}" alt=""></span>
+                            <p class="ml-3">2 Huespedes</p>
+                        </div>
+                        
+                        <h2 class="text-4xl font-semibold tracking-wide mb-[5rem] font-playfair">
+                            Habitación Estándar Sencilla
+                        </h2>
+
+                        <ul class="list-disc text-sm pl-4 mb-[5rem]">
+                            <li class="mt-2">1 cama matrimonial</li>
+                            <li class="mt-2">TV por cable</li>
+                            <li class="mt-2">Teléfono</li>
+                            <li class="mt-2">Wifi</li>
+                            <li class="mt-2">Aire acondicionado</li>
+                            <li class="mt-2">Baño completo con todas sus amenidades</li>
+                        </ul>
+
+                        <div>
+                            <button class="text-xs font-semibold uppercase border border-white rounded-full px-5 py-3 hover:text-primary-dark hover:bg-white transition-colors duration-200">Reservar ahora</button>
+                        </div>
+
+                    </div>
+                </div>
+                <div class="w-1/2">
+                    <div class="w-full h-screen">
+                        <img class="w-full h-screen object-cover" src="{{asset('images/habitaciones/habitacion-doble-sencilla-02.jpg')}}" alt="Habitacion doble queen">
+                    </div>
+                </div>
+            </div>
+        </div>
+                
+        <div class="swiper-slide">
+            <div class="flex flex-row-reverse justify-between items-start min-h-screen">
+                <div class="bg-primary-dark w-1/2 text-primary-light">
+                    <div class="w-full h-screen pl-20 pt-[6.2rem]">
+
+                        <div class=" flex items-baseline text-[.6rem] text-primary-gray mb-6">
+                            <span><img src="{{asset('icons/Line.svg')}}" alt=""></span>
+                            <p class="ml-3">6 Huespedes</p>
+                        </div>
+                        
+                        <h2 class="text-4xl font-semibold tracking-wide mb-[5rem] font-playfair">
+                            Habitación Familiar
+                        </h2>
+
+                        <ul class="list-disc text-sm pl-4 mb-[5rem]">
+                            <li class="mt-2">1 cama king size</li>
+                            <li class="mt-2">2 cama queen size</li>
+                            <li class="mt-2">TV por cable</li>
+                            <li class="mt-2">Teléfono</li>
+                            <li class="mt-2">Wifi</li>
+                            <li class="mt-2">Aire acondicionado</li>
+                            <li class="mt-2">Baño completo con todas sus amenidades</li>
+                        </ul>
+
+                        <div>
+                            <button class="text-xs font-semibold uppercase border border-white rounded-full px-5 py-3 hover:text-primary-dark hover:bg-white transition-colors duration-200">Reservar ahora</button>
+                        </div>
+
+                    </div>
+                </div>
+                <div class="w-1/2">
+                    <div class="w-full h-screen">
+                        <img class="w-full h-screen object-cover" src="{{asset('images/habitaciones/habitacion-familiar-06.jpg')}}" alt="Habitacion doble queen">
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
         <!-- Slide Navigation -->
     <div class="prevRoomEl absolute bottom-16 right-[40%] z-10 cursor-pointer w-fit" id="prev-room-btn">
