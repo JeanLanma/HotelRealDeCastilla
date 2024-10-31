22df63fc5497e77df610d66a8d3d1539fc8076d6
Jean <jeanmacario048@gmail.com>
2024-10-31 13:16:45 -0600

    Add\ LG screen version

--- resources/views/Front/Home/_partials/hero.blade.php
+++ resources/views/Front/Home/_partials/hero.blade.php
@@ -1,38 +1,38 @@
 <div class="bg-[url('images/hero-home.png')] bg-contain bg-no-repeat w-full min-h-screen">
     <div class="mx-auto max-w-7xl px-24 flex flex-col justify-center items-start">
-        <div class="mt-60">
+        <div class="mt-44 xl:mt-60">
             <div class="text-white">
-                <p class="text-4xl">
+                <p class="text-3xl xl:text-4xl">
                     Bienvenido a
                 </p>
                 <h1 class="font-playfair font-bold">
-                    <span class="text-[6.2rem] tracking-widest leading-snug">
+                    <span class="text-7xl xl:text-[6.2rem] tracking-widest leading-snug">
                         Real de Castilla 
                     </span>
                     
-                    <small class="block text-5xl tracking-widest -mt-8">H O T E L E S</small>
+                    <small class="block lg:text-4xl xl:text-5xl tracking-widest -mt-4 xl:-mt-8">H O T E L E S</small>
                 </h1>
-                <p class="text-2xl font-normal leading-6 pt-4">
+                <p class="xl:text-2xl font-normal xl:leading-6 xl:pt-4">
                     Reserva en línea y conoce el <br> centro histórico de Guadalajara
                 </p>
             </div>
         </div>
-        <div class="pt-20 mx-auto rounde-md">
-            <button onclick="moveToElement('locations', 100)" class="py-4 px-10 rounded-lg bg-acent-blue text-white font-bold uppercase flex items-center">
-                <div class="w-6">
+        <div class="pt-16 xl:pt-20 mx-auto rounde-md">
+            <button onclick="moveToElement('locations', 100)" class="py-2 px-5 xl:py-4 xl:px-10 rounded-lg bg-acent-blue text-white font-semibold xl:font-bold uppercase flex items-center">
+                <div class="w-4 xl:w-6">
                     <img class="w-full" src="{{asset('icons/book-now.svg')}}" alt="Book now icon">
                 </div>
-                <p class="ml-4 text-lg">
+                <p class="ml-4 text-sm xl:text-lg">
                     Reserva ahora
                 </p>
             </button>
         </div>
-        <div class="mx-auto py-14">
+        <div class="mx-auto py-7 xl:py-14">
             <div onclick="moveToElement('locations', 100)" class="flex flex-col items-center justify-center cursor-pointer">
-                <p class="text-white font-bold text-lg">
+                <p class="text-white font-semibold xl:font-bold text-sm xl:text-lg">
                     Ver más
                 </p>
-                <div class="w-12 mt-4">
+                <div class="w-10 xl:w-12 mt-4">
                     <img id="ScrollDownHeroElement" src="{{asset('icons/Subtraction 2.svg')}}" alt="schroll down">
                 </div>
             </div>
--- resources/views/Front/_partials/menu.blade.php
+++ resources/views/Front/_partials/menu.blade.php
@@ -1,16 +1,16 @@
 <header class="relative">
-    <div class="absolute top-0 left-0 py-0 px-40 min-w-full flex justify-center {{ !request()->routeIs('home') ? 'bg-primary-blue' : '' }}">
+    <div class="absolute top-0 left-0 py-0 px-24 xl:px-40 min-w-full flex justify-center {{ !request()->routeIs('home') ? 'bg-primary-blue' : '' }}">
         <div class="w-full max-w-7xl flex justify-between items-center relative top-0 left-0 py-0 2xl:px-12 sm:h-28">
             <div>
-                <div class="bg-primary-blue h-[10.6rem] absolute -top-4 flex items-center rounded-b-[3rem]">
-                    <a href="{{ route('home') }}" class="block w-[17rem] px-4">
+                <div class="bg-primary-blue h-32 xl:h-[10.6rem] absolute -top-4 flex items-center rounded-b-[3rem]">
+                    <a href="{{ route('home') }}" class="block w-52 xl:w-[17rem] px-4">
                         <img class="w-full" src="{{ asset('images/logo-texto.png') }}" alt="Logo Hotel" />
                     </a>
                 </div>
             </div>
             <div>
                 <nav>
-                    <ul class="flex justify-between gap-x-[3.2rem] text-white font-medium text-[1.2rem] leading-6">
+                    <ul class="flex justify-between gap-x-6 xl:gap-x-[3.2rem] text-white font-medium xl:text-[1.2rem] leading-6">
                         <li class="px-1 hover:border-b-2 {{ request()->routeIs('home') ? 'border-b-2 font-bold' : '' }}">
                             <a href="{{ route('home') }}">Inicio</a>
                         </li>
