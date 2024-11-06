35e6dd422416d9ebc65c2125d18452c248374570
Jean <jeanmacario048@gmail.com>
2024-11-06 16:15:54 -0600

    WO\ Landing Footer about section responsive

--- resources/views/Front/Landing/Home/_partials/rooms.blade.php
+++ resources/views/Front/Landing/Home/_partials/rooms.blade.php
@@ -2,11 +2,14 @@
     <div class="mt-12">
         <div>
             <div>
-                <h3 class="text-3xl 2xl:text-5xl text-primary-blue font-bold font-playfair">Conoce nuestras habitaciones</h3>
+                <h3 class="ml-6 sm:ml-0 text-3xl 2xl:text-5xl text-primary-blue font-bold font-playfair">
+                    Conoce nuestras
+                    <span class="text-golden-yellow">habitaciones</span>
+                </h3>
             </div>
         </div>
         <div>
-            <div class="mt-11 flex gap-x-4">
+            <div class="mt-11 px-6 sm:px-0 flex flex-col sm:flex-row gap-x-4">
                 <div class="flex flex-col gap-y-5 2xl:gap-y-7">
                     <div>
                         <img src="{{asset('images/meet-room-1.png')}}" alt="Hab 1">
@@ -16,7 +19,7 @@
                     </div>
                 </div>
                 
-                <div>
+                <div class="my-6 sm:my-0">
                     <div>
                         <img src="{{asset('images/meet-room-3.png')}}" alt="Hab 3">
                     </div>
