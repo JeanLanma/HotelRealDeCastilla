a2dfca58b81c4ed0b81924736e7d9153fcd6f564
Jean <jeanmacario048@gmail.com>
2024-11-07 17:06:14 -0600

    Build\ colonial home page hero section 07.11.2024-1706

--- resources/views/Front/Colonial/Home/_partials/Hero.blade.php
+++ resources/views/Front/Colonial/Home/_partials/Hero.blade.php
@@ -1,13 +1,13 @@
 <section>
     <div class="bg-[url('images/hero-home.png')] bg-cover w-full h-calc-menu flex">
         
-        <div class="max-w-7xl px-7 lg:px-14 mx-auto w-full flex flex-col justify-between mt-24 lg:mt-56">            
+        <div class="max-w-7xl px-7 lg:px-14 mx-auto w-full flex flex-col justify-between mt-20 xl:mt-56">            
             <div>
                 <div class="text-white">
                     <h1 class="text-2xl font-bold tracking-wide mb-7">
-                        Hotel real de Castilla Colonial
+                        Hotel Real de Castilla <span class="text-golden-yellow">Colonial</span>
                     </h1>
-                    <p class="lg:text-lg lg:font-playfair lg:font-bold w-60">
+                    <p class="lg:text-lg lg:font-playfair lg:font-bold w-64">
                         Reserva en línea y conoce el centro histórico de Guadalajara
                     </p>
                 </div>
@@ -15,10 +15,10 @@
 
             <div class="self-center -mb-12 sm:mb-0">
                 <div>
-                    <div class="text-white bg-primary-dark w-64 md:w-[50rem] text-xs font-semibold tracking-wide px-9 py-7">
+                    <div class="text-white bg-primary-dark w-64 md:w-[50rem] text-xs font-semibold tracking-wide px-9 py-9 lg:py-7">
                         <div class="flex flex-col sm:flex-row justify-center items-center gap-x-14">
                         
-                            <div id="GuestsTrigger" class="w-full mb-10 sm:mb-0 sm:w-auto flex items-start gap-4 hover:cursor-pointer hover:text-primary-orange">
+                            <div id="GuestsTrigger" class="w-full mb-12 sm:mb-0 sm:w-auto flex items-start gap-4 hover:cursor-pointer hover:text-primary-orange">
                                 <div>
                                     <img src="{{asset('icons/guest.svg')}}" alt="Icono Huesped">
                                 </div>
@@ -33,7 +33,7 @@
                                 </div>
                             </div>
                             
-                            <div id="DateCheckInTrigger" class="w-full mb-10 sm:mb-0 sm:w-auto flex items-start gap-4 hover:cursor-pointer hover:bg-white/30 transition-colors duration-200">
+                            <div id="DateCheckInTrigger" class="w-full mb-12 sm:mb-0 sm:w-auto flex items-start gap-4 hover:cursor-pointer hover:bg-white/30 transition-colors duration-200">
                                 <div>
                                     <img src="{{asset('icons/calendar.svg')}}" alt="Icono Huesped">
                                 </div>
@@ -49,7 +49,7 @@
                                 </div>
                             </div>
 
-                            <div id="DateCheckOutTrigger" class="w-full mb-10 sm:mb-0 sm:w-auto flex items-start gap-4">
+                            <div id="DateCheckOutTrigger" class="w-full mb-12 sm:mb-0 sm:w-auto flex items-start gap-4">
                                 <div>
                                     <img src="{{asset('icons/calendar.svg')}}" alt="Icono Huesped">
                                 </div>
@@ -65,7 +65,7 @@
                                 </div>
                             </div>
 
-                            <div>
+                            <div class="mt-3 sm:mt-0">
                                 <button id="BookingWidgetBtn" class="bg-primary-orange text-white font-semibold py-5 px-10 rounded-[5px] hover:bg-primary-dark ring-2 ring-primary-orange ring-inset transition-colors duration-150">
                                     Book now
                                 </button>
