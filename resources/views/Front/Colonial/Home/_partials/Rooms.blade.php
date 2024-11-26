339a59de779c5788d528d1c370493235420290ee
Jean <jeanmacario048@gmail.com>
2024-11-26 17:29:36 -0600

    BUILD\ Responsive partials fix home overflow 26.11.2024-1729

--- resources/views/Front/Colonial/Home/_partials/Rooms.blade.php
+++ resources/views/Front/Colonial/Home/_partials/Rooms.blade.php
@@ -2,9 +2,9 @@
     <div class="swiper-wrapper">
 
         <div class="swiper-slide">
-            <div class="flex flex-col-reverse sm:flex-row-reverse justify-between items-start min-h-screen">
+            <div class="flex flex-col-reverse sm:flex-row-reverse justify-between items-start sm:min-h-screen">
                 <div class="bg-primary-dark sm:w-1/2 text-primary-light">
-                    <div class="w-full h-screen pl-7 pt-7 sm:pl-20 sm:pt-[6.2rem]">
+                    <div class="w-full pb-24 sm:pb-0 sm:h-screen pl-7 pt-7 sm:pl-20 sm:pt-[6.2rem]">
 
                         <div class=" flex items-baseline text-[.6rem] text-primary-gray mb-6">
                             <span><img src="{{asset('icons/Line.svg')}}" alt=""></span>
@@ -225,7 +225,7 @@
         </div>
                 
         <div class="swiper-slide">
-            <div class="flex flex-row-reverse justify-between items-start min-h-screen">
+            <div class="flex flex-row-reverse justify-between items-start sm:min-h-screen">
                 <div class="bg-primary-dark w-1/2 text-primary-light">
                     <div class="w-full h-screen pl-20 pt-[6.2rem]">
 
@@ -255,7 +255,7 @@
                     </div>
                 </div>
                 <div class="w-1/2">
-                    <div class="w-full h-screen">
+                    <div class="w-full sm:h-screen">
                         <img class="w-full h-screen object-cover pane-ltr" src="{{asset('images/habitaciones/habitacion-familiar-06.jpg')}}" alt="Habitacion doble queen">
                     </div>
                 </div>
@@ -263,10 +263,10 @@
         </div>
     </div>
         <!-- Slide Navigation -->
-    <div class="prevRoomEl absolute bottom-32 sm:bottom-16 right-[60%] sm:right-[40%] z-10 cursor-pointer w-fit" id="prev-room-btn">
+    <div class="prevRoomEl absolute bottom-3 sm:bottom-16 right-[60%] sm:right-[40%] z-10 cursor-pointer w-fit" id="prev-room-btn">
         <svg id="prevRoomEl" fill="#ffffff" width="64px" height="64px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" stroke="#ffffff"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g id="Circle_Chev_Left" data-name="Circle Chev Left"> <g> <path d="M10.15,12.35a.492.492,0,0,1,0-.7l3-3a.495.495,0,0,1,.7.7L11.21,12l2.64,2.65a.495.495,0,0,1-.7.7Z"></path> <path d="M12,21.933A9.933,9.933,0,1,1,21.933,12,9.944,9.944,0,0,1,12,21.933ZM12,3.067A8.933,8.933,0,1,0,20.933,12,8.943,8.943,0,0,0,12,3.067Z"></path> </g> </g> </g></svg>
     </div>
-    <div class="nextRoomEl absolute bottom-32 sm:bottom-16 right-1/3 z-10 cursor-pointer w-fit" id="next-room-btn">
+    <div class="nextRoomEl absolute bottom-3 sm:bottom-16 right-1/3 z-10 cursor-pointer w-fit" id="next-room-btn">
         <svg id="nextRoomEl" fill="#ffffff" width="64px" height="64px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" stroke="#ffffff"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g id="Circle_Chev_Right" data-name="Circle Chev Right"> <g> <path d="M13.85,11.65a.492.492,0,0,1,0,.7l-3,3a.495.495,0,0,1-.7-.7L12.79,12,10.15,9.35a.495.495,0,0,1,.7-.7Z"></path> <path d="M12,2.067A9.933,9.933,0,1,1,2.067,12,9.944,9.944,0,0,1,12,2.067Zm0,18.866A8.933,8.933,0,1,0,3.067,12,8.943,8.943,0,0,0,12,20.933Z"></path> </g> </g> </g></svg>
     </div>
     <!-- /Slide Navigation -->
